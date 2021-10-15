e6399701c6b15c8345d9ff472f194db4e7909b84
John Sam <johnsd2709@gmail.com>
2021-10-15 14:20:52 +0530

    Edited

--- src/App.js
+++ src/App.js
@@ -18,7 +18,7 @@ const app=new Clarifai.App({
 const particlesOptions={
   particles: {
     number: {
-      value:40,
+      value:10,
       density:{
         enable:true,
         value_area:800
@@ -34,8 +34,25 @@ class App extends Component{
       imageUrl:'',
       box:{},
       route:'Signin',
-      isSignedin:false
+      isSignedin:false,
+      user:{
+        id:'',
+        name:'',
+        email:'',
+        entries:0,
+        joined:''
+      }
     }
+  }
+
+  loadUser=(data)=>{
+    this.setState({user:{
+      id:data.id,
+      name:data.name,
+      email:data.email,
+      entries:data.entries,
+      joined:data.joined
+    }})
   }
 
   calculateFaceLocation=(data)=>{
@@ -53,7 +70,6 @@ class App extends Component{
   }
 
   displayFaceBox = (box)=>{
-    console.log(box);
     this.setState({box:box});
   }
 
@@ -64,7 +80,22 @@ class App extends Component{
   onButtonSubmit=()=>{
     this.setState({imageUrl:this.state.input});
     app.models.predict(Clarifai.FACE_DETECT_MODEL,this.state.input)
-    .then(response => this.displayFaceBox(this.calculateFaceLocation(response)))
+    .then(response => {
+      if(response){
+        fetch('http://localhost:3000/image',{
+          method:'put',
+          headers:{'Content-type':'application/json'},
+          body:JSON.stringify({
+            id:this.state.user.id
+          })
+      })
+        .then(response=>response.json())
+        .then(count=>{
+          this.setState(Object.assign(this.state.user,{entries:count}))
+        })
+      }
+      this.displayFaceBox(this.calculateFaceLocation(response))
+    })
     .catch(err=>console.log(err));
   }
 
@@ -74,7 +105,7 @@ class App extends Component{
   }else if(route==='home'){
     this.setState({isSignedin:true})
   }
-  this.setState({route:route})
+  this.setState({route:route});
 }
 
   render(){
@@ -87,14 +118,14 @@ class App extends Component{
         {route==='home'
           ?<div>
             <Logo/>
-            <Rank/>
+            <Rank name={this.state.user.name} entries={this.state.user.entries}/>
             <ImageLinkForm onInputChange={this.onInputChange} onButtonSubmit={this.onButtonSubmit}/>
             <FaceRecognition box={box} imageUrl={imageUrl}/>
         </div>
           :(
             route==='Signin'
-            ? <Signin onRouteChange={this.onRouteChange}/>
-            : <Register onRouteChange={this.onRouteChange}/>
+            ? <Signin loadUser={this.loadUser} onRouteChange={this.onRouteChange}/>
+            : <Register loadUser={this.loadUser} onRouteChange={this.onRouteChange}/>
           )
          
         }
--- src/Components/Signin/Signin.js
+++ src/Components/Signin/Signin.js
@@ -1,37 +1,84 @@
 import React from 'react';
 
-const Signin=({onRouteChange})=>{
-    return(
-        <article className="br3 ba dark-gray b--black-10 mv4 w-100 w-50-m w-25-l mw6 shadow-5 center"> 
-            <main className="pa4 black-80">
-                <div className="measure">
-                <fieldset id="sign_up" className="ba b--transparent ph0 mh0">
-                    <legend className="f1 fw6 ph0 mh0">Sign In</legend>
-                    <div className="mt3">
-                    <label className="db fw6 lh-copy f6" htmlFor="email-address">Email</label>
-                    <input className="pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100" type="email" name="email-address"  id="email-address" />
-                    </div>
-                    <div className="mv3">
-                    <label className="db fw6 lh-copy f6" htmlFor="password">Password</label>
-                    <input className="b pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100" type="password" name="password"  id="password" />
-                    </div>
-                </fieldset>
-                <div className="">
-                    <input 
-                        onClick={()=>onRouteChange('home')}
-                        className="b ph3 pv2 input-reset ba b--black bg-transparent grow pointer f6 dib" 
-                        type="submit" 
-                        value="Sign in" 
-                    />
-                </div>
-                <div className="lh-copy mt3">
-                    <p onClick={()=> onRouteChange('Register')}  className="f6 link dim black db pointer">Register</p>
-                </div>
-                </div>
-            </main>
-      </article>
-    );
+class Signin extends React.Component{
+    constructor(props){
+        super(props);
+        this.state={
+            SigninEmail:'',
+            SigninPassword:''
+        }
+    }
 
+    onEmailChange=(event)=>{
+        this.setState({SigninEmail:event.target.value})
+    }
+    onPasswordChange=(event)=>{
+        this.setState({SigninPassword:event.target.value})
+    }
+
+    onsubmitSignIn=()=>{
+        fetch('http://localhost:3000/Signin',{
+            method:'post',
+            headers:{'Content-type':'application/json'},
+            body:JSON.stringify({
+                email:this.state.SigninEmail,
+                password:this.state.SigninPassword
+            })
+        })
+         .then(response=>response.json())
+          .then(user =>{
+             if(user.id){
+                this.props.loadUser(user);
+                this.props.onRouteChange('home');
+             }
+         })
+    }
+
+    render(){
+        const{onRouteChange}=this.props;
+        return(
+            <article className="br3 ba dark-gray b--black-10 mv4 w-100 w-50-m w-25-l mw6 shadow-5 center"> 
+                <main className="pa4 black-80">
+                    <div className="measure">
+                    <fieldset id="sign_up" className="ba b--transparent ph0 mh0">
+                        <legend className="f1 fw6 ph0 mh0">Sign In</legend>
+                        <div className="mt3">
+                        <label className="db fw6 lh-copy f6" htmlFor="email-address">Email</label>
+                        <input 
+                            className="pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100" 
+                            type="email" 
+                            name="email-address"  
+                            id="email-address" 
+                            onChange={this.onEmailChange}
+                        />
+                        </div>
+                        <div className="mv3">
+                        <label className="db fw6 lh-copy f6" htmlFor="password">Password</label>
+                        <input 
+                            className="b pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100" 
+                            type="password" 
+                            name="password"  
+                            id="password" 
+                            onChange={this.onPasswordChange}
+                        />
+                        </div>
+                    </fieldset>
+                    <div className="">
+                        <input 
+                            onClick={this.onsubmitSignIn}
+                            className="b ph3 pv2 input-reset ba b--black bg-transparent grow pointer f6 dib" 
+                            type="submit" 
+                            value="Sign in" 
+                        />
+                    </div>
+                    <div className="lh-copy mt3">
+                        <p onClick={()=> onRouteChange('Register')}  className="f6 link dim black db pointer">Register</p>
+                    </div>
+                    </div>
+                </main>
+            </article>
+        );
+    }
 }
 
 export default Signin;
